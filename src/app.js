4c14f397cf94c48a05a4340c78508d2ad807ef75
Mohammad <m4design@gmail.com>
2017-06-05 21:20:08 +0300

    split reducers and use combine reducers

--- src/app.js
+++ src/app.js
@@ -1,32 +1,42 @@
-const store = Redux.createStore(function (state, action) {
-    switch (action.type) {
-        case 'ADD_CARD':
-            let newCard = Object.assign({}, action.data, {
-                score: 1,
-                id: +new Date
-            });
 
-            return Object.assign({}, state, {
-                cards: state.cards ? state.cards.concat([newCard]) : [newCard]
-            });
-        default:
-            return state || {};
-    }
-});
+const cards = (state, action) => {
+  switch (action.type) {
+    case 'ADD_CARD':
+      let newCard = Object.assign({}, action.data, {
+        score: 1,
+        id: +new Date
+      });
+
+      return state.concat([newCard]);
+    default:
+      return state || [];
+  }
+};
+
+const store = Redux.createStore(Redux.combineReducers({
+  cards
+}));
+
+// function (state, action) {
+//   return {
+// cards: cards(state.cards, action),
+// decks: cards(state.decks, action)
+//   };
+// });
 
 store.subscribe(() => {
-    console.log(store.getState());
+  console.log(store.getState());
 });
 
 store.dispatch({
-    type: 'ADD_CARD',
-    data: {
-        front: 'front',
-        back: 'back'
-    }
+  type: 'ADD_CARD',
+  data: {
+    front: 'front',
+    back: 'back'
+  }
 });
 
 store.dispatch({
-    type: 'ADD_CARD',
-    date: {}
+  type: 'ADD_CARD',
+  date: {}
 });
