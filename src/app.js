44ad823fdd0cf5cf121ca571926bfa6c5279b1a0
Mohammad <m4design@gmail.com>
2017-06-06 03:57:41 +0300

    use action creators

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+const addDeck = name => ({ type: 'ADD_DECK', data: name });
+const showAddDeck = () => ({ type: 'SHOW_ADD_DECK' });
+const hideAddDeck = () => ({ type: 'HIDE_ADD_DECK' });
+
 const cards = (state, action) => {
   switch (action.type) {
     case 'ADD_CARD':
@@ -12,8 +16,28 @@ const cards = (state, action) => {
   }
 };
 
+const decks = (state, action) => {
+  switch (action.type) {
+    case 'ADD_DECK':
+      let newDeck = { name: action.data, id: +new Date };
+      return state.concat([newDeck]);
+    default:
+      return state || [];
+  }
+};
+
+const addingDeck = (state, action) => {
+  switch (action.type) {
+    case 'SHOW_ADD_DECK': return true;
+    case 'HIDE_ADD_DECK': return false;
+    default: return !!state;
+  }
+};
+
 const store = Redux.createStore(Redux.combineReducers({
-  cards
+  cards,
+  decks,
+  addingDeck
 }));
 
 const App = (props) => {
@@ -25,22 +49,46 @@ const App = (props) => {
 };
 
 const Sidebar = React.createClass({
+  componentDidUpdate() {
+    var el = ReactDOM.findDOMNode(this.refs.add);
+    if (el) {
+      el.focus();
+    }
+  },
   render() {
-    let props = this.props;
-
     return (
       <div className="sidebar">
         <h2>All Decks</h2>
+        <button onClick={this.props.showAddDeck}>New Deck</button>
         <ul>
-          {props.decks.map((deck, i) =>
+          {this.props.decks.map((deck, i) =>
             <li key={i}>{deck.name}</li>
           )}
         </ul>
-        {props.addingDeck && <input ref="add" />}
+        {this.props.addingDeck && <input ref="add" onKeyPress={this.createDeck} />}
       </div>);
+  },
+  createDeck(evt) {
+    if (evt.which !== 13) return;
+    var name = ReactDOM.findDOMNode(this.refs.add).value;
+    this.props.addDeck(name);
+    this.props.hideAddDeck();
   }
 });
 
-ReactDOM.render(<App>
-  <Sidebar decks={[{ name: "Deck 1" }]} addingDeck={true} />
-</App>, document.getElementById('root'));
+function run() {
+  let state = store.getState();
+
+  ReactDOM.render(<App>
+    <Sidebar decks={state.decks}
+      addingDeck={state.addingDeck}
+      addDeck={name => store.dispatch(addDeck(name))}
+      showAddDeck={() => store.dispatch(showAddDeck())}
+      hideAddDeck={() => store.dispatch(hideAddDeck())}
+    />
+  </App>, document.getElementById('root'));
+}
+
+run();
+
+store.subscribe(run);
